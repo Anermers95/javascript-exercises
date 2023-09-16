9e6fbf65e89773b5b0a2fbe73533ccf32640fc14
Edward <edwardfoosw@gmail.com>
2023-09-16 20:33:53 +0800

    lab 01,02,03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+
+    let result = '';
+    if (num < 0) return 'ERROR';
+
+    else {
+        for (let i = 0; i < num; i++) {
+            result += word
+        }
+    }
+
+    return (result);
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+
+    if (word == "")
+        return "";
+
+    let result = word.split("")
+    result = result.reverse();
+    result = result.join("");
+
+    return result;
 
 };
 
