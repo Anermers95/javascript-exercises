4cdf9bcb1980d8435afc1aedf0a342a17469c9d4
Edward <edwardfoosw@gmail.com>
2023-09-17 15:29:37 +0800

    lab 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+
+    let result = (5 / 9) * (a - 32);
+
+    result = Math.round(result * 10) / 10
+
+    return result;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a) {
+    let result = a * (9 / 5) + 32
+
+    result = Math.round(result * 10) / 10
+
+
+    return result;
+
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
